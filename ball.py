a20b2b370eda0d2ae67f5df738fcda637b727e54
kwa_laa <khoaddnguyen@gmail.com>
2023-08-21 00:34:51 -0500

    Detect collision with paddles and shoot new balls to immediate winning player

--- ball.py
+++ ball.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
-import random
+
+
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
@@ -14,6 +15,14 @@ class Ball(Turtle):
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def bounce(self):
+    def bounce_y(self):
         # ball goes the opposite way of current direction
         self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        # new ball bounces to the other player
+        self.bounce_x()
--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 # 7. Detect when paddle misses
 # 8. Keep score
 
-from turtle import Screen, Turtle
+from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 import time
@@ -24,8 +24,8 @@ screen.tracer(0)
 # while game_is_on:
 #   screen.update()
 
-r_paddle = Paddle((350,0))
-l_paddle = Paddle((-350,0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 ball = Ball()
 
 screen.listen()
@@ -44,10 +44,20 @@ while game_is_on:
     screen.update()
     ball.move()
 
-# 5. Detect collision with wall and bounce
+# 5. Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
 
-# 6. Detect collision with paddle
+# 6. Detect collision with paddles
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+# 7. Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+# 7. Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
