299ddede345efba135a408a1f9902f5ccc28ff3d
kwa_laa <khoaddnguyen@gmail.com>
2023-08-21 10:41:52 -0500

    Add scoreboards. Increase speed each time ball hits a paddle.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 # 1. Create the screen
@@ -27,6 +28,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 
@@ -40,7 +42,7 @@ screen.onkey(l_paddle.go_down, "s")
 # 4. Create ball and make it move
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -52,12 +54,16 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-# 7. Detect R paddle misses
+# 7A - 8. Detect R paddle misses + score
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
-# 7. Detect L paddle misses
+# 7B - 8. Detect L paddle misses + score
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
+
+
 
 screen.exitonclick()
